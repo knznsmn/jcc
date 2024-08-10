dd90cc8bf3d59508aeb9c71db230823d45fdcb89
Julius Cinco Cesar <knznsmn@icloud.com>
2024-08-10 15:09:43 +0400

    Make Pulse middle gradient color prominent

--- app/services/design/page.jsx
+++ app/services/design/page.jsx
@@ -10,7 +10,7 @@ const Design = () => {
   return (
     <main>
       <section className={styles.container}>
-        <article>
+        <article className='gridlines'>
           <Heading title='Designing Your Website' />
         </article>
       
