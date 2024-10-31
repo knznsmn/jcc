448b5eb815ed7e3ad8649daa182cc4fd037776a6
Julius Cinco Cesar <knznsmn@icloud.com>
2024-10-31 05:58:37 +0400

    Commit in a hurry

--- components/(home)/Grille/index.jsx
+++ components/(home)/Grille/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from './Grille.module.css'
 import Contact from '@components/Contact'
 
@@ -11,9 +12,14 @@ const Grille = () => {
 				</section>
 				<section className={styles.resum}>
 					<h4>My résumé</h4>
+					<div>
+						<Link href="https://cv.jccesar.com" alt="Julius Cinco Cesar's résumé">
+							<i className='icon-apache'></i>
+						</Link>
+					</div>
 				</section>
 				<section className={styles.proje}>
-					<h3>Projects</h3>
+					<h4>Projects</h4>
 				</section>
 				<section className={styles.about}>
 					<h4>More About Me</h4>
@@ -23,13 +29,32 @@ const Grille = () => {
 				</section>
 				<section className={styles.tools}>
 					<h4>Tools</h4>
+					<div>
+						<i className="icon-html"></i>
+						<i className="icon-css"></i>
+						<i className="icon-javascript"></i>
+						<i className="icon-nginx"></i>
+						<i className="icon-apache"></i>
+						<i className="icon-adobephotoshop"></i>
+						<i className="icon-adobeillustrator"></i>
+						<i className="icon-adobepremiere"></i>
+						<i className="icon-terminal"></i>
+						<i className="icon-git"></i>
+						<i className="icon-github"></i>
+					</div>
 				</section>
 				<section className={styles.socia}>
-
+					<p>@knznsmn</p>
+					<div>
+						<Link href="https://www.facebook.com/knznsmn" target='_blank'><i className="icon-facebook"></i></Link>
+						<Link href="https://www.linkedin.com/in/knznsmn" alt="LinkedIn profile" target="_blank"><i className="icon-linkedin"></i></Link>
+						<Link href="https://www.instagram.com/knznsmn" alt="Instagram profile" target="_blank"><i className="icon-instagram"></i></Link>
+						<Link href="https://www.twitter.com/knznsmn" alt="X profile" target="_blank"><i className="icon-twitter"></i></Link>
+					</div>
 				</section>
 				<section className={styles.conta}>
 					<p>
-						Have a project in mind?
+						Mail me!
 					</p>
 					<Contact />
 				</section>
