393ba31cd6174f6ebac74ab006822f34f39beba2
Julius Cinco Cesar <knznsmn@icloud.com>
2024-11-02 07:24:18 +0400

    Tweak /about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -14,26 +14,27 @@ export default function About() {
   return (
     <main>
       <section className={styles.container}>
-        <article>
-          <Heading title='About Me' />
-        </article>
 
         <article>
           <Introduction />
         </article>
 
+        <article>
+          <Heading title='About Me' />
+        </article>
+
         <article className={styles.left}>
           <section>
             <h3>Hi there!</h3>
-            <p>Good <Moment />. I'm Julius Cinco Cesar. I love coffee and coffee jokes.</p>
+            <p>Good <Moment />. I'm Julius Cinco Cesar. I love learning and creating stuff.</p>
             <Image src={signature} alt="Signature" />
           </section>
 
           <section> 
-          <h2>I Volunteer!</h2>
-          <p>
-            I’m on a mission to gain hands-on experience and give back to the community through volunteering. If you’re passionate about creating positive change and have a volunteer opportunity that needs enthusiastic support, I’m all ears! Let’s connect and make a meaningful impact together.
-          </p>
+            <h2>I Volunteer!</h2>
+            <p>
+              I’m on a mission to gain hands-on experience and give back to the community through volunteering. If you’re passionate about creating positive change and have a volunteer opportunity that needs enthusiastic support, I’m all ears! Let’s connect and make a meaningful impact together.
+            </p>
           </section>
         </article>
 
