3aaf9b57e34cbc8dbddc69e1e0d6e982e06b66a5
Julius Cinco Cesar <knznsmn@icloud.com>
2024-11-04 09:27:06 +0400

    Update Footer right

--- components/(home)/Services/index.jsx
+++ components/(home)/Services/index.jsx
@@ -3,9 +3,8 @@ import styles from './Services.module.css'
 const Services = () => {
   return (
     <ul className={styles.services}>
-      <li><a href="/services/design">Design.</a></li>
-      <li><a href="/services/develop">Develop.</a></li>
-      <li><a href="/services/deploy">Deploy.</a></li>
+      <li><a href="https://www.github.com/knznsmn/jcc"><i className="icon-github"></i> Source</a></li>
+      <li><a href="/notes"><i className="icon-pencil"></i> Notes</a></li>
     </ul>
   )
 }
